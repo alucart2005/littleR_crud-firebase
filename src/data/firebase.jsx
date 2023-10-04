75cd31e974b77f204a7756ada3e6adfc68e4bcac
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-10-04 09:55:38 -0500

    feature: start firebase config

---
+++ src/data/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCfUJoMd80xtw5E5VDxSgoEVDlzhiYXxwc",
+  authDomain: "fir-crud-289f1.firebaseapp.com",
+  projectId: "fir-crud-289f1",
+  storageBucket: "fir-crud-289f1.appspot.com",
+  messagingSenderId: "684143787764",
+  appId: "1:684143787764:web:acffb2de3fd6194bdfa8a1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
