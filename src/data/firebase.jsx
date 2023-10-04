e5c711687a35947f0c009715b6bcec6ad7c8ed30
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-10-04 14:22:39 -0500

    2

--- src/data/firebase.jsx
+++ src/data/firebase.jsx
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyCfUJoMd80xtw5E5VDxSgoEVDlzhiYXxwc",
-  authDomain: "fir-crud-289f1.firebaseapp.com",
-  projectId: "fir-crud-289f1",
-  storageBucket: "fir-crud-289f1.appspot.com",
-  messagingSenderId: "684143787764",
-  appId: "1:684143787764:web:acffb2de3fd6194bdfa8a1"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+    apiKey: "AIzaSyCfUJoMd80xtw5E5VDxSgoEVDlzhiYXxwc",
+    authDomain: "fir-crud-289f1.firebaseapp.com",
+    projectId: "fir-crud-289f1",
+    storageBucket: "fir-crud-289f1.appspot.com",
+    messagingSenderId: "684143787764",
+    appId: "1:684143787764:web:acffb2de3fd6194bdfa8a1",
+  },
+  app = initializeApp(firebaseConfig),
+  db = getFirestore(app);
+export { db };
