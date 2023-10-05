3905fa100bbdaa0f5915100b7947cc0dcaddc572
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-10-04 19:21:08 -0500

    feature: button complete

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,18 @@
-
-
-export function Todo({todo,handleDelete,toggleComplete}) {
+export function Todo({ todo, handleDelete, toggleComplete }) {
   return (
     <div className="todo">
-      <input style={{textDecoration:todo.completed && "line-through"}}
-      value={todo.title} />
+      <input
+        style={{ textDecoration: todo.completed && "line-through" }}
+        value={todo.title}
+      />
       <div>
-        <button onClick={()=>toggleComplete(todo)}>Completar</button>
-        <button onClick={()=>handleDelete(todo.id)}>Delete</button>
+        <button
+          className="button-complete"
+          onClick={() => toggleComplete(todo)}
+        >
+          Mark
+        </button>
+        <button onClick={() => handleDelete(todo.id)}>Delete</button>
       </div>
     </div>
   );
